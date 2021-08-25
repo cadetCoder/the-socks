5d0ae2a75f1226be236694f12e225f4eb547f9b9
Darrel Toledo <darreltoledo@icloud.com>
2021-08-25 21:30:59 +0100

    v-bind attribute changed

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: '#app',
   product: 'Socks',
-  image:'./assets/vmSocks-green-onWhite.jpeg',
+  image:'./assets/socks-green.jpeg',
+  altText:'A pair of socks',
   description: 'A pair of warm, fuzzy socks'
 })
