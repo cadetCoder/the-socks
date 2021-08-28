1df102ecafc57047ac35f925c35f58cba0a6ea73
Darrel Toledo <darreltoledo@icloud.com>
2021-08-28 21:52:33 +0100

    debug

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var app = new Vue({
   el: '#app',
   product: 'Socks',
   image:'./assets/socks-green.jpeg',
-  altText:'A pair of socks',
   description: 'A pair of warm, fuzzy socks',
   inStock: true,
   details: ["80% cotton", "20% polyester", "Gender Neutral"],
