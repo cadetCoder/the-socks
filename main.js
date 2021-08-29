1067b2ef7a546866d3478837370d505b78cce001
Darrel Toledo <darreltoledo@icloud.com>
2021-08-29 22:34:08 +0100

    debug image now works

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var app = new Vue({
   el: '#app',
+  data: {
   product: 'Socks',
   image:'./assets/socks-green.jpeg',
   description: 'A pair of warm, fuzzy socks',
@@ -15,4 +16,5 @@ var app = new Vue({
       variantColor: "blue"
     }
   ]
+  }
 })
