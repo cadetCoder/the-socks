2c3d95a20a24d22d2697c9c3fa5e4d0db89ae7cf
Darrel Toledo <darreltoledo@icloud.com>
2021-09-02 22:06:07 +0100

    brand added to data

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 var app = new Vue({
   el: '#app',
   data: {
-  product: 'Socks',
-  image:'./assets/socks-green.jpeg',
-  inStock: true,
-  details: ["80% cotton", "20% polyester", "Gender Neutral"],
-  variants: [
-    {
-      variantId: 2234,
-      variantColor: "green",
-      variantImage: "./assets/socks-green.jpeg"
-    },
-    {
-      variantId: 2235,
-      variantColor: "blue",
-      variantImage: "./assets/socks-blue.jpeg"
-    }
-  ],
+    brand:'Vue Mastery',
+    product: 'Socks',
+    image:'./assets/socks-green.jpeg',
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender Neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/socks-green.jpeg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/socks-blue.jpeg"
+      }
+    ],
   cart: 0
   },
   methods: {
