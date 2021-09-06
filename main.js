3c62208df2d3734557d53b1441b9b8d44f94325b
Darrel Toledo <darreltoledo@icloud.com>
2021-09-06 22:26:59 +0100

    HTML turned into component

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+vue.component('product',{
+  template:`
+    <div class="product">
+    <div class="product-image">
+      <img :src="image" />
+    </div>
+
+    <div class="product-info">
+      <h1>{{ title }}</h1>
+      <p v-if="inStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      
+      <ul>
+        <li v-for="detail in details">{{ detail }} </li>
+      </ul>
+    
+
+    <div v-for="(variant, index) in variants"
+        :key="variant.variantId"
+        class="color-box"
+        :style="{ backgroundColor: variant.variantColor}"
+        @mouseover="updateProduct(index)">
+    </div>
+
+    <button v-on:click="addToCart"
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }">Add to Cart</button>
+
+    <div class="cart">
+      <p>Cart ({{ cart }})</p>
+    </div>
+
+  </div>
+  </div>
+  `
+})
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -42,12 +79,4 @@ var app = new Vue({
       return this.variants[this.selectedVariant].variantQuantity
     }
   }
-})
-
-vue.component('product',{
-  template:`
-    <div class="product">
-      ...
-    </div>
-  `
 })
